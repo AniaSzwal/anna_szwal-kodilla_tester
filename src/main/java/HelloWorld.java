81ce969f7afbac1fd60517c944575111af9c61e2
Anna Szwal <aniaszwal@gmai.com>
2021-01-03 00:58:45 +0100

    HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("Hello World! Anna S, lat 23"); // [3]
+    }
+}
